e21f796e0fd1c27c3b91a3ae493ff0a553a91f1f
lei <teargle@163.com>
2022-11-04 17:03:51 +0800

    保存多语言使用直接写入

--- application/index/model/I18n.php
+++ application/index/model/I18n.php
@@ -59,7 +59,7 @@ class I18n extends Model
 
     public function updateI18n( $id, $text ) {
         $text = addslashes($text) ;
-        return Db::query( "update dn_i18n set text=:text where id=:id " , [ 'id' => $id , 'text' => $text ] );
+        return Db::query( "update dn_i18n set text='{$text}' where id={$id} ");
     }
  
 }
