ef28256ca69a115e892110c54361b95338691d27
lei <teargle@163.com>
2022-11-04 17:22:52 +0800

    保存多语言使用直接写入

--- application/index/model/I18n.php
+++ application/index/model/I18n.php
@@ -53,12 +53,10 @@ class I18n extends Model
     }
 
     public function saveI18n( $table, $column, $lang, $target_id, $text ) {
-        $text = addslashes($text) ;
         return Db::query( "insert into dn_i18n (`table`,`column`,`lang`,`target_id`,`text` ) values ('{$table}', '{$column}', '{$lang}', {$target_id}, '{$text}') " );
     }
 
     public function updateI18n( $id, $text ) {
-        $text = addslashes($text) ;
         $str = "text = '" . $text . "'" ;
         return Db::query( "update dn_i18n set {$str} where id={$id} ");
     }
